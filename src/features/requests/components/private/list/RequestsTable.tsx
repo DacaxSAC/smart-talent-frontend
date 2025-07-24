2d1d079eeb3ef3069b761f1e30b5823a1ce34702
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-07-24 09:10:10 -0500

    fix(requests): conditionally render add button and view button based on status
    Only show add button when request is not confirmed in CreationTable
    Only show view button for users or recruiters with specific statuses in RequestsTable

--- src/features/requests/components/private/list/RequestsTable.tsx
+++ src/features/requests/components/private/list/RequestsTable.tsx
@@ -329,13 +329,15 @@ export const RequestsTable = ({ data, isLoading, isError, loadingText, errorText
                 </div>
               </div>
               <div className="col-span-2 p-2  hidden md:block text-center">
-                <button
-                  title="Ver detalles de solicitud"
-                  className="cursor-pointer text-center hover:text-main-2plus border border-white-1 px-1 rounded-[5px]"
-                  onClick={() => openResourceModal(index)}
-                >
-                  <p>Ver</p>
-                </button>
+                {(isUser || (isRecruiter && (request.status === 'IN_PROGRESS' || request.status === 'OBSERVED')) )&& (
+                  <button
+                    title="Ver detalles de solicitud"
+                    className="cursor-pointer text-center hover:text-main-2plus border border-white-1 px-1 rounded-[5px]"
+                    onClick={() => openResourceModal(index)}
+                  >
+                    <p>Ver</p>
+                  </button>
+                )}
                 {isRecruiter && request.status === 'PENDING' && (
                   <button 
                     className="cursor-pointer bg-success text-white py-0.5 px-1 rounded-[5px] ml-2"
