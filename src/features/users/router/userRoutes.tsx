abf80634c9b6d27698068ec106c09b5817996fff
DacacodeX <ducz.daca@icloud.com>
2025-07-29 21:36:31 -0500

    feat(users): add user routes and update user page components
    Implement user management routes including list, create and update pages
    Add UpdateUserPage component with form handling for both natural and juridical user types

---
+++ src/features/users/router/userRoutes.tsx
@@ -0,0 +1,20 @@
+import { ROLES } from "@/features/auth/constants/roles";
+import { CreateUserPage } from "../pages/CreateUserPage";
+import { ListUsersPage } from "../pages/ListUsersPage";
+import { UpdateUserPage } from "../pages/UpdateUserPage";
+
+export const userRoutes = [
+  {
+    path: '/users',
+    element: <ListUsersPage />,
+  },
+  {
+    path: '/users/create',
+    element: <CreateUserPage />,
+    roles: [ROLES.ADMIN]
+  },{
+    path: '/users/edit/:id',
+    element: <UpdateUserPage />,
+    roles: [ROLES.ADMIN]
+  }
+];
